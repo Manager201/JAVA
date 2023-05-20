b433706cbd9bd1cd369223fcf70e472628fe7744
Manager201 <shivendramaurya1108@gmail.com>
2023-05-20 23:51:43 +0530

    expanded form pf any number

---
+++ expanded.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class expanded
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        int  d,s,c=0,p,m=n;
+        while(m!=0){
+            p=m%10;
+            c++;
+            m=m/10;
+        }
+        c=c-1;
+        while(c!=0)
+    {
+        
+        d=n%(int)Math.pow(10,c);
+        s=n-d;
+        n=d;
+        System.out.print(s+"+");
+        c--;
+        
+    }
+    System.out.print(n);
+
+    }
+    }
