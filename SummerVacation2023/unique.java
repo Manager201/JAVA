e5d1db1e99db20f124c3a1bd5ed66cb1518f3d26
Manager201 <82263454+Manager201@users.noreply.github.com>
2023-05-26 11:06:49 +0530

    unique number

---
+++ SummerVacation2023/unique.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class unique {
+    public static void main(String[] args) {
+        int n, c = 0, d;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        n = sc.nextInt();
+        
+        while (n != 0) {
+            d = n % 10;
+            n = n / 10;
+            int x = n;
+            while (x != 0) {
+                if (x % 10 == d)
+                    c++;
+                x = x / 10;
+            }
+
+        }
+        if (c == 0)
+            System.out.println("Unique Number");
+        else
+            System.out.println("Not a Unique number");
+    }
+}
