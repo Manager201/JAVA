56a4a1604ff9824479b53165a91b10ebc0dcfe83
Manager201 <82263454+Manager201@users.noreply.github.com>
2024-12-18 12:06:23 +0000

    1

--- Winter vacation 2025/cyclo.java
+++ Winter vacation 2025/cyclo.java
@@ -4,9 +4,15 @@ class cyclo
     public static void main(String[] args) 
         {
             Scanner sc=new Scanner(System.in);
-            System.out.println("Enter a number");
+            System.out.println("Enter a number:");
             int a=sc.nextInt();
             String a1=Integer.toString(a);
-            System.out.println(a);
+            int l=a1.length(),c=0;
+            if(a1.charAt(0)==a1.charAt(l-1))
+            c++;
+            if(c==1)
+            System.out.println(a1+" is a cyclo number");
+            else
+            System.out.println(a1+" not a cyclo number");
         }
     }
