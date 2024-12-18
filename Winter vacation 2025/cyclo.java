495b04c0437d807e999d3421821fe1bf3127e894
Manager201 <82263454+Manager201@users.noreply.github.com>
2024-12-18 11:41:00 +0000

    1

---
+++ Winter vacation 2025/cyclo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class cyclo
+{
+    public static void main(String[] args) 
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter a number");
+            int a=sc.nextInt();
+            String a1=Integer.toString(a);
+            System.out.println(a);
+        }
+    }
