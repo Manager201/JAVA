4a7cfb8620266475cebde3f3b341124fee848d6f
Manager201 <82263454+Manager201@users.noreply.github.com>
2024-12-19 09:39:44 +0000

    1

---
+++ Winter vacation 2025/fascinating.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class fascinating {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int a, i = 0, n, m, d, c = 0, e = 0;
+        String w = "";
+        a = sc.nextInt();
+        for (i = 1; i <= 3; i++) {
+            w = w + Integer.toString(a * i);
+        }
+        n = Integer.parseInt(w);
+        m = n;
+        for (i = 0; i <= 9; i++) {
+            while (n != 0) {
+                d = n % 10;
+                if (d == i)
+                    c++;
+                n = n / 10;
+            }
+            if (c > 1)
+                e++;
+            n = m;
+            c = 0;
+        }
+        if (e > 0)
+            System.out.println(a + " is not a fascinating number");
+        else
+            System.out.println(a + " is a fascinating number");
+
+    }
+}
