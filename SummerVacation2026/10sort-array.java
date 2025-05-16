b2a01f99e7602717ca40bcd5926294a1dd758f89
Manager201 <82263454+Manager201@users.noreply.github.com>
2025-05-16 20:14:52 +0000

    V1

--- SummerVacation2026/10sort-array.java
+++ SummerVacation2026/10sort-array.java
@@ -4,3 +4,10 @@ Example- Input: a [ ] = {1, 2, 4, 7, 10, 12, 13} Yes, Ascending Order
 Input: a [ ] = {13, 10, 7, 4} Yes, Descending Order
 Input: a [ ] = {6, 9, 2, 5, 8} Not sorted
  */
+import java.util.*;
+class sort_array
+{
+    public static void main(String[] args) {
+        
+    }
+}
--- SummerVacation2026/20day-number.java
+++ SummerVacation2026/20day-number.java
@@ -25,5 +25,107 @@ Example 4: Input: Day Number = 366
 Year =2009
 Output:
 Invalid Day Number!
-
  */
+import java.util.*;
+class day_number
+ 
+{
+    int[] m = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    String[] mn = { "0", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec" };
+
+    public String check(int dn, int y) 
+    {
+        if (y % 4 == 0)
+            m[2] = 29;
+
+        int s = 0, d = 0;
+        String mon = "", date = "";
+        for (int i = 1; i < m.length; i++) 
+        {
+            s =s+ m[i];
+            if (s >= dn) 
+            {
+                mon = mn[i];
+                d = dn - (s - m[i]);
+                break;
+            }
+        }
+
+        switch (d % 10) 
+        {
+            case 1:
+                if (d == 11)
+                    date = d + "th";
+                else
+                    date = d + "st";
+                break;
+            case 2:
+                if (d == 12)
+                    date = d + "th";
+                else
+                    date = d + "nd";
+                break;
+            case 3:
+                if (d == 13)
+                    date = d + "th";
+                else
+                    date = d + "rd";
+                break;
+            default:
+                date = d + "th";
+        }
+
+        return date + " " + mon + " " + y;
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Day Number = ");
+        int dn = sc.nextInt();
+        if(dn>366||dn<1)
+        {
+            System.out.print("Invalid Date");
+            System.exit(0);
+        }
+        System.out.print("Year = ");
+        int y = sc.nextInt();
+
+        if (y % 4 != 0 && dn == 366) 
+        {
+            System.out.println("Invalid Day Number!");
+            System.exit(0);
+        }
+
+        System.out.print("Date before (n) = ");
+        int n = sc.nextInt();
+
+        if (y < 1000 || y > 9999) 
+        {
+            System.out.println("Invalid year");
+            System.exit(0);
+        }       
+        if (n < 0) 
+        {
+            System.out.println("Invalid date");
+            System.exit(0);
+        }
+
+        pg11_date obj = new pg11_date();
+        String out1 = obj.check(dn, y);
+        System.out.println(out1);
+
+        int newdn = dn - n;
+        if (n > dn) 
+        {
+            System.out.println("Date before " + n + " days does not lie in the same year.");
+            System.exit(0);
+        }
+        else
+        {
+        String out2 = obj.check(newdn, y);
+        System.out.println("Date before " + n + " days = " + out2);
+        }
+    }
+}
