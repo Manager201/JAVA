f4aba14f0cff71dc2bcf34286e23184918245511
Manager201 <82263454+Manager201@users.noreply.github.com>
2025-05-16 15:51:13 +0000

    v1

--- SummerVacation2026/05smith.java
+++ SummerVacation2026/05smith.java
@@ -102,10 +102,10 @@ static int primefact_sum(int a)
                 String tstr="";
                 while (t!=0) {
                     int d=t%10;
-                    if(t==i)
-                    tstr=" + "+d;
+                    if (t==i)
+                    tstr=Integer.toString(d);
                     else
-                    tstr=d+tstr;
+                    tstr=d+" + "+tstr;
                     t=t/10;
                 }
                 sof=sof+"(" +tstr+")"+" = "+sum;
@@ -119,3 +119,9 @@ static int primefact_sum(int a)
     return sum;
     }
 }
+/*
+sample outputs from 1 to 1000 :
+4 22 27 58 85 94 121 166 202 265 274 319 346 355 378 382 391 
+438 454 483 517 526 535 562 576 588 627 634 636 645 648 654 663 
+666 690 706 728 729 762 778 825 852 861 895 913 915 922 958 985
+ */
---
+++ SummerVacation2026/06composite_magic.java
@@ -0,0 +1,81 @@
+/*
+A composite Magic number is a positive integer which is composite as well as a magic number.
+Composite Number : A composite number is a number that has more than two factors.
+Example- 10, 12 etc.
+Magic Number : A magic number is a number in which the eventual sum of the digits is equal to 1.
+Example- 37 = 3 + 7 = 10 = 1 + 0 = 1
+Accept two positive integers m and n (assume m<n). Display the number of Composite Magic integers that are in the range
+between m and n and output them along with frequency, in the format specified below.
+Example- Input: m=20, n=95
+Output: The Composite Magic integers are: 28, 46, 55, 64, 82, 91
+Frequency of Composite Magic integers is = 6
+ */
+import java.util.*;
+class composite_magic
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter lower limit 'm' : ");
+        int m,n,i,count=0;
+        String magicString="";
+        m=sc.nextInt();
+        System.out.println("Enter higher limit 'n' : ");
+        n=sc.nextInt();
+        if(m<0||n<0)
+        {
+            System.out.println("Retry with a positive number");
+            System.exit(0);
+        }
+        if(m>n)
+        {
+        System.out.println("Invalid lower and higher limit");
+        System.exit(0);
+        }
+        for(i=m+1;i<n;i++)
+        {
+            if(iscomposite(i)&&find_magic(i))
+            {
+            magicString=magicString+i+", ";
+            count++;
+            }
+        }
+        System.out.println("The Composite Magic integers are: "+(magicString.substring(0, magicString.length() - 2)));
+        System.out.println("Frequency of Composite Magic integers is = "+count);
+    }
+    static boolean iscomposite(int a)
+    {
+        int i,count=0;
+        for(i=1;i<=a;i++)
+        if(a%i==0)
+        count++;
+        if(count>2)
+        return true;
+        else
+        return false;
+    }
+    static boolean find_magic(int a)
+    {
+        int s=0,d;
+        while(a>9)
+        {
+            while(a!=0)
+            {
+                d=a%10;
+                s=s+d;
+                a=a/10;
+            }
+            a=s;
+            s=0;
+        }
+        if(a==1)
+        return true;
+        else
+        return false;
+    }
+}
+/*
+sample outputs from 1 to 500 :
+The Composite Magic integers are: 10, 28, 46, 55, 64, 82, 91, 100, 118, 136, 145, 154, 172, 190, 
+208, 217, 226, 235, 244, 253, 262, 280, 289, 298, 316, 325, 334, 343, 352, 361, 370, 388, 406, 415, 
+424, 442, 451, 460, 469, 478, 496
+ */
