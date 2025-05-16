5f4dcf692390df23587d71e9f04c819da4fd0e50
Manager201 <82263454+Manager201@users.noreply.github.com>
2025-05-16 11:27:58 +0000

    v1

--- SummerVacation2026/04prime-adam.java
+++ SummerVacation2026/04prime-adam.java
@@ -5,3 +5,45 @@ Adam number: The Square of a number and the square of its reverse are reverse to
 Example- 1. Input: m = 13
 Output: Yes! Prime-Adam
 */
+import java.util.*;
+class prime_adam{
+    public static void main(String[] args) {           
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number");
+    int n;
+    n=sc.nextInt();
+    if(n<0)
+    {
+        System.out.println("Retry again with a positive number.");
+        System.exit(0);
+    }
+    if(reverse(n*n)==(reverse(n)*reverse(n))&&isprime(n)==true)
+    System.out.println("Yes! Prime-Adam");
+    else
+    System.out.println("No! Not Prime-Adam");
+    }
+    static boolean isprime(int a)
+    {
+        int i,count=0;
+        for(i=1;i<=a;i++)
+        if(a%i==0)
+        count++;
+        if(count==2)
+        return true;
+        else 
+        return false;
+    }
+    static int reverse(int a)
+    {
+        int d,r=0;
+        while(a!=0)
+        {
+        d=a%10;
+        r=r*10+d;
+        a=a/10;
+        }
+        return r;
+    }
+}
+//sample outputs from 1 to 1000:
+//2, 3, 11, 13, 31, 101, 103, 113, 211, 311 
