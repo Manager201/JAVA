17eefeae7c1b0edd60228cea939be42ecd3f1d31
Manager201 <82263454+Manager201@users.noreply.github.com>
2025-05-16 18:05:40 +0000

    v1

--- SummerVacation2026/08unique-digit.java
+++ SummerVacation2026/08unique-digit.java
@@ -9,3 +9,50 @@ The Unique digit numbers are:
 102, 103, 104, 105, 106, 107, 108, 109
 Frequency of unique digit numbers is: 8
  */
+import java.util.*;
+class unique_digit {
+    public static void main(String[] args) {
+        int a, b, count, freq = 0, d, m, n, x, i;
+        String print = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter lower limit 'm':");
+        m = sc.nextInt();
+        System.out.println("Enter higher limit 'n':");
+        n = sc.nextInt();
+
+        if (m > n) {
+            System.out.println("Invalid lower and higher limit");
+            System.exit(0);
+        }
+
+        for (i = m; i <= n; i++) {
+            a = i;
+            count = 0;
+            b = a;
+
+            while (b != 0) {
+                d = b % 10;
+                b = b / 10;
+                x = b;
+                while (x != 0) {
+                    if (x % 10 == d)
+                        count++;
+                    x = x / 10;
+                }
+            }
+
+            if (count == 0) {
+                print = print + i + ", ";
+                freq++;
+            }
+        }
+
+        System.out.println("The Unique digit numbers are:");
+        if (freq > 0)
+            System.out.println(print.substring(0, print.length() - 2));
+        else
+            System.out.println("None");
+
+        System.out.println("Frequency of unique digit numbers is: " + freq);
+    }
+}
