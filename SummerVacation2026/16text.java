8d043ac488e10bf10ff2dfd503e8762cf123dafe
Manager201 <82263454+Manager201@users.noreply.github.com>
2025-05-17 10:05:04 +0000

    v1

--- SummerVacation2026/16text.java
+++ SummerVacation2026/16text.java
@@ -6,3 +6,35 @@ Emotions, controlled and directed to work, is character, By Swami Vivekananda
 Output:
 Vivekananda Swami By character is work to directed and controlled Emotions
  */
+import java.util.*;
+class text
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String str,str2="",w="";
+        int len,i;
+        str=sc.nextLine();
+        str=str.replace(",","");
+        str=str+" ";
+        len=str.length();
+        for(i=0;i<len;i++)
+        {
+            char ch=str.charAt(i);
+            if(ch!=' ')
+                w=w+ch;
+            else
+            {
+            str2=w+" "+str2;
+            w="";
+            }
+        }
+        System.out.println(str2);
+    }
+}
+/*
+terminal output :
+Enter a sentence:
+Java, programming language, is fun and powerful
+powerful and fun is language programming Java
+ */
