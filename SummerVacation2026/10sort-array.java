884c6530bc953dbda69039f2f19af0d9f31e5bc7
Manager201 <82263454+Manager201@users.noreply.github.com>
2025-05-17 07:14:46 +0000

    v1

--- SummerVacation2026/10sort-array.java
+++ SummerVacation2026/10sort-array.java
@@ -8,6 +8,43 @@ import java.util.*;
 class sort_array
 {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of an array : ");
+        int size,i;
+        String array="";
+        size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter array elements:");
+        for(i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        array=array+"{";
+        for(i=0;i<size;i++)
+        {
+            array=array+arr[i]+", ";
+        }
+        System.out.println(array.substring(0,array.length()-2)+"} "+sort(arr));
+
+    }
+    static String sort(int a[])
+    {
+        int i,desc=0,asc=0;
+        for(i=0;i<a.length-1;i++)
+        if(a[i]>=a[i+1]==false)
+        {
+            if(a[i]<=a[i+1])
+            {
+              asc++;
+            }
+        }
+        else 
+        desc++;
+        if(asc>0&&desc>0)
+        return "Not sorted";
+        if(asc>0&&desc==0)
+        return "Yes, Ascending Order";
+        else
+        return "Yes, Descending Order";
     }
 }
