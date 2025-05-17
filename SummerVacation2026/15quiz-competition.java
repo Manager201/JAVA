1bcbf8fa47eb87645a9497d74720d3c8d731e437
Manager201 <82263454+Manager201@users.noreply.github.com>
2025-05-17 21:05:01 +0000

    v1

--- SummerVacation2026/15quiz-competition.java
+++ SummerVacation2026/15quiz-competition.java
@@ -21,3 +21,63 @@ Participant 4
 2) Input: n = 13
 Output: Input size is out of range.
  */
+import java.util.*;
+class quiz_competition
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of participants: ");
+        int n,i,j,count=0,l;
+        n=sc.nextInt();
+        sc.nextLine();
+        if(n<3||n>11)
+        {
+            System.out.println("Input size is out of range.");
+            System.exit(0);
+        }
+        char arr[][]=new char[n][5];
+        char key[]={'A', 'C', 'D', 'B', 'B'};
+        int score[]=new int[n];
+        for(i = 0; i < n; i++) 
+        {
+            System.out.print("Participant " + (i + 1) + "  ");
+            for(j = 0; j < 5; j++) 
+                    arr[i][j] = sc.next().charAt(0);
+        }
+        System.out.println("Scores:");
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n+1;j++)
+            {
+                if(key[j]==arr[i][j])
+                count++;
+            }
+            score[i]=count;
+            System.out.println("Participant "+(i+1)+" = "+count);
+            count=0;
+        }
+        System.out.println("Highest score:");
+        l=score[0];
+        for(i=0;i<n;i++)
+        {
+            if(l<=score[i])
+            System.out.println("Participant "+(i+1));
+        }
+    }
+}
+/*
+Terminal output :
+Enter the number of participants: 4
+Participant 1: A C C B D
+Participant 2: B C A A C
+Participant 3: B C B A A
+Participant 4: C C D D B
+Scores:
+Participant 1 = 3
+Participant 2 = 1
+Participant 3 = 1
+Participant 4 = 3
+Highest score:
+Participant 1
+Participant 4
+ */
